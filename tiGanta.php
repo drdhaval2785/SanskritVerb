e16ae718652c3d2cf765bd522310ea22b750982f
Dr. Dhaval Patel <drdhaval2785@gmail.com>
2015-10-19 09:54:34 +0530

    Issue 195 closed
    https://github.com/drdhaval2785/SanskritVerb/issues/195

--- tiGanta.php
+++ tiGanta.php
@@ -2,7 +2,7 @@
  /* This code is developed by Dr. Dhaval Patel (drdhaval2785@gmail.com) of www.sanskritworld.in and Ms. Sivakumari Katuri.
   * Layout assistance by Mr Marcis Gasuns.
   * Available under GNU licence.
-  * Version 1.0.2 date 17 October 2015
+  * Version 1.1.0 date 18 October 2015
   * The latest source code is available at https://github.com/drdhaval2785/sanskrit
   * Acknowledgements: I extend my heartfelt thanks to Ananda Loponen for the code to convert devanagari and various sanskrit transliterations. That can be accessed at http://www.ingmardeboer.nl/php/diCrunch.php?act=help.
   * I also extend my gratitude to gloomy.penguin of stackoverflow.com, who helped me create dvitva and lopa functions, without which I would be handicapped.
@@ -2566,7 +2566,7 @@ if ($lakAra==="liw" && arr($text,'/^['.pc('hl').']/'))
     display(0);
 }*/
 /* liTi dhAtoranabhyAsasya (6.1.8) */
-if ($lakAra==="liw" && arr($text,'/^['.pc('ac').']/'))
+elseif ($lakAra==="liw" && arr($text,'/^['.pc('ac').']/'))
 {
 	liT_ajAdi(); // pending to refractor the function.
 	$abhyAsa=1;
