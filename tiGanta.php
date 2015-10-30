7f8b7703d2bd9a4479cce4e3671d59623dc507b2
Dr. Dhaval Patel <drdhaval2785@gmail.com>
2015-10-30 22:39:44 +0530

    Issue 269 closed
    https://github.com/drdhaval2785/SanskritVerb/issues/269

--- tiGanta.php
+++ tiGanta.php
@@ -2365,8 +2365,13 @@ if (  in_array($lakAra,array("luw")) && pr2(array("+"),blank(0),$tiG1,array("+")
 /* luTaH prathamasya DAraurasaH (2.4.85) */
 if ( in_array($lakAra,array("luw")) && $tAs===1 && pr2(array("+"),array("tAs+"),array("ti","tas","anti","te","Ate","ante"),array("+"),array("tAss+"),$tiG1,$text)!==$text )
 {
-    $text=pr2(array("+"),array("tAs+"),array("ti","tas","anti","te","Ate","ante"),array("+"),array(""),array("tA","tAs+rO","tAs+ras","tA","tAs+rO","tAs+ras",),$text);
+    $text=pr2(array("+"),array("tAs+"),array("ti","tas","anti","te","Ate","ante"),array("+"),array(""),array("tAs+qA","tAs+rO","tAs+ras","tA","tAs+rO","tAs+ras",),$text);
 	storedata('2.4.85','sa',0);
+	storedata('1.3.7','pa',0);	
+	$text=one(array("tAs+qA"),array("tAs+A"),0);
+	storedata('1.3.9','sa',0);
+	$text=one(array("tAs+A"),array("tA"),0);
+	storedata('6.4.143','sa',0);
 	$syatAsI=1;
 }
 /* tAsastyorlopaH (7.4.50) */
