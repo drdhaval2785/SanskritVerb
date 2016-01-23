a0e388466e7cc6e9a9c19dfe933940c913cc5979
drdhaval2785 <drdhaval2785@gmail.com>
2016-01-23 12:02:58 +0530

    Issue 409 closed
    https://github.com/drdhaval2785/SanskritVerb/issues/409

--- panini.php
+++ panini.php
@@ -5065,13 +5065,16 @@ if (arr($text,'/A\+/') && sub(array("A"),array("+"),$halAdi_apit_sArvadhAtuka_pr
     $text=three(array("A"),array("+"),$halAdi_apit_sArvadhAtuka_pratyayas,array("I"),array("+"),$halAdi_apit_sArvadhAtuka_pratyayas,0);
 	storedata('6.4.113','sa',0);
 }
+print_r($text);
 /* znA'bhyastayorAtaH (6.4.112) */
 // For znA
+$text = two(array("+nA+Iy+"),$ac,array("+nA+Iy"),$ac,0);
 if (arr($text,'/\+nA\+/') && sub(array("+nA"),array("+"),$apit_sArvadhAtuka_pratyayas,0) && (in_array("N",$it)||in_array("k",$it)) && in_array("SnA",$vik))
 {
     $text=three(array("+nA"),array("+"),$apit_sArvadhAtuka_pratyayas,array("n"),array("+"),$apit_sArvadhAtuka_pratyayas,0);
 	storedata('6.4.112','sa',0);
 }
+print_r($text);
 /* janasanakhanAM saJjhaloH (6.4.42) */
 // sannanta pending.
 //if ( sub(array("jan","san","Kan"),array("+"),prat("Jl"),0) && pr2(array("jan","san","Kan"),array("+"),$halAdi_apit_sArvadhAtuka_pratyayas,array("jaA","saA","KaA"),array("+"),$halAdi_apit_sArvadhAtuka_pratyayas,$text)!==$text && (in_array("N",$it)||in_array("k",$it)) && in_array($so,$tiG) ) // kGiti is temporarily deactivated.
