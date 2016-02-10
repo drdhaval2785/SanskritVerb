1c547c0738a6ea8f5dd4ea4474d6b87fa0f0bb8a
drdhaval2785 <drdhaval2785@gmail.com>
2016-02-10 14:53:43 +0530

    Issue 494 closed
    https://github.com/drdhaval2785/SanskritVerb/issues/494

--- panini.php
+++ panini.php
@@ -1146,15 +1146,23 @@ if (in_array($lakAra,array("law","low","laN","viDiliN","sArvaDAtukalew"))  && su
     $sarvadhatuka=1;
 	storedata('3.4.113','pa',0);
 }
-/* ArdhadhAtukaM zeSaH (3.4.114) */ 
-if ( in_array($lakAra,array("lfw","lfN","luw","ASIrliN","luN","ArDaDAtukalew")) && sub(array("+"),$tiG,blank(0),0) )
+/* liGAziSi (3.4.116) */ 
+if ( in_array($lakAra,array("ASIrliN")) )
 {
     $ardhadhatuka=1;
+	storedata('3.4.116','pa',0);
+}
+/* ArdhadhAtukaM zeSaH (3.4.114) */ 
+if ( in_array($lakAra,array("lfw","lfN","luw","luN","ArDaDAtukalew")) )
+{
+    $ardhadhatuka=1;
+	storedata('3.4.114','pa',0);
 }
 /* liT ca (3.4.115) */ 
-if ( in_array($lakAra,array("liw")) && sub(array("+"),$tiG,blank(0),0) )
+if ( in_array($lakAra,array("liw")) )
 {
     $ardhadhatuka=1;
+	storedata('3.4.115','pa',0);
 }
 /* Adding vikaraNas */
 $vik=array();
@@ -4287,16 +4295,6 @@ if ($lakAra==="liw" && $so==="sip" && arr($text,'/[iIuUfFxXeEoO]\+/') && sub(pra
 }
 /* patch for yAsuT Agama to combine it with the next pratyaya. because it would not be getting iDAgama. */
 $text = one(array("+yA+"),array("+yA"),0);
-/* ArdhadhAtukaM zeSaH (3.4.114) */ 
-if ( in_array($lakAra,array("lfw","lfN","luw","ASIrliN","luN","ArDaDAtukalew"))  )
-{
-	storedata('3.4.114','pa',0);
-}
-/* liT ca (3.4.115) */ 
-if ( in_array($lakAra,array("liw")) )
-{
-	storedata('3.4.115','pa',0);
-}
 /* Adding iDAgama actually */
 if ($id_dhAtu==="sew" && $id_pratyaya==="sew" && !($yAsuT===1 && $lakAra==="ASIrliN")  && !in_array("iw",$Agama) && $caG!==1 && $ksa!==1) // for seT dhAtus
 {
