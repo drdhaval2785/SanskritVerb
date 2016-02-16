11e2bef4004c57ebf7137f207c29e39a7f63b53a
drdhaval2785 <drdhaval2785@gmail.com>
2016-02-16 10:18:17 +0530

    allsutrani gzipped
    To reduce the size of the tar file

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ git clone https://github.com/drdhaval2785/SanskritVerb
 cd SanskritVerb/Data
 #echo "downloading allsutrani.tar 35 MB..."
 #curl -o allsutrani.tar http://www.sanskritdocuments.org/learning_tools/sarvanisutrani/allsutrani.tar
+#gzip allsutrani.tar
 mkdir allsutrani
-tar -xvf allsutrani.tar -C allsutrani
+tar -zxvf allsutrani.tar.gz -C allsutrani
 tar zxvf dhaatupatha.tgz
