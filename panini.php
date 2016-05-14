e93cbfeed32eca6b07c3073ffb65a8c1a33e7fb6
drdhaval2785 <drdhaval2785@gmail.com>
2016-05-14 17:51:34 +0530

    DUz->DUs done. Generic analysis pending
    https://github.com/drdhaval2785/SanskritVerb/issues/798

--- panini.php
+++ panini.php
@@ -7399,6 +7399,7 @@ if ( $lakAra!=="" && arr($text,'/Dv/') && sub(array("s"),array("Dv"),blank(0),0)
     $text = two(array("s"),array("Dv"),array(""),array("Dv"),0);
 	storedata('8.2.25','sa',0);
 }
+print_r($text);
 /* AdezapratyayayoH (8.3.59) */
 if( in_array($so,$tiG) && arr($text,'/([iIuUfFxXeEoOhyvrlkKgGN])([+]*[iI]*)s(['.pc('al').'])/') && !arr($text,'/[+][s]$/') && arr($text,'/s/') && ( $rudAdibhyaH===1 || $SaHsaH===1 || $sic===1 || $sic===2 || $syatAsI===1 || ends(array($us),prat('ik'),1) || $sIyuT===1 || ($lakAra==="liw" && arr($text,'/[+]ise$/')) ) && !arr($text,'/\+yAs\+/') && !(arr(array($fo),'/^s/') && arr($text,'/^[^+]*[iIuUfFxeEoOhyvrlkKgGN][+]s/')) )
 {
@@ -7420,10 +7421,11 @@ if( in_array($so,$tiG) && arr($text,'/([iIuUfFxXeEoOhyvrlkKgGN])([+]*[iI]*)s(['.
 	{
 		$text = change('/rI[+]z/','rI+s');
 	}
+	echo $number;
 	// kusma dhAtu has 's' in upadeza. Therefore, reverting it back.
-	if (arr($text,'/kuzm/'))
+	if (in_array($number,array("10.0139","10.0236")) && sub(array("kuzm","DUz"),blank(0),blank(0),0))
 	{
-		$text = change('/kuzm/','kusm');
+		$text = one(array("kuzm","DUz"),array("kusm","DUs"),0);
 	}
 	if ($in!==$text )
 	{
@@ -7431,6 +7433,7 @@ if( in_array($so,$tiG) && arr($text,'/([iIuUfFxXeEoOhyvrlkKgGN])([+]*[iI]*)s(['.
 		$Adezapratyaya=1;		
 	}
 }
+print_r($text);
 /* vibhASeTaH (8.3.79) */
 if( in_array($so,$tiG) && (arr($text,'/[iIuUfFxeoEOhyvrl]\+izIDv/') || (arr($text,'/[iIuUfFxeoEOhyvrl]\+iDv['.pc('al').']+$/') && in_array($lakAra,array("luN","liw")) ))  )
 {
