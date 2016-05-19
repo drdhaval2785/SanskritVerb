4e9e648064bc9f6a0531bd3030b5c16f5a0dbf58
drdhaval2785 <drdhaval2785@gmail.com>
2016-05-19 17:15:09 +0530

    AdezapratyayayoH
    sisekise->sisekize

--- panini.php
+++ panini.php
@@ -7501,6 +7501,7 @@ if ( $lakAra!=="" && arr($text,'/Dv/') && sub(array("s"),array("Dv"),blank(0),0)
     $text = two(array("s"),array("Dv"),array(""),array("Dv"),0);
 	storedata('8.2.25','sa',0);
 }
+print_r($text);
 /* AdezapratyayayoH (8.3.59) */
 if( in_array($so,$tiG) && arr($text,'/([iIuUfFxXeEoOhyvrlkKgGN])([+]*[iI]*)s(['.pc('al').'])/') && !arr($text,'/[+][s]$/') && arr($text,'/s/') && ( $rudAdibhyaH===1 || arr($text,'/[iIuUfFxeEoOhyvrlkKgGN][+]si$/') || $SaHsaH===1 || $sic===1 || $sic===2 || $syatAsI===1 || ends(array($us),prat('ik'),1) || $sIyuT===1 || ($lakAra==="liw" && arr($text,'/[+]ise$/')) ) && !arr($text,'/\+yAs\+/') && !(arr(array($fo),'/^s/') && arr($text,'/^[^+]*[iIuUfFxeEoOhyvrlkKgGN][+]s/')) )
 {
@@ -7533,9 +7534,9 @@ if( in_array($so,$tiG) && arr($text,'/([iIuUfFxXeEoOhyvrlkKgGN])([+]*[iI]*)s(['.
 		$Adezapratyaya=1;		
 	}
 }
-elseif (arr($text,'/[iIuUfFxeEoOhyvrlkKgGN][+]si$/') && $so==="sip")
+elseif (arr($text,'/[iIuUfFxeEoOhyvrlkKgGN][+][i]*s[ie]$/') && in_array($so,array("sip","TAs")))
 {
-	$text = change('/([iIuUfFxeEoOhyvrlkKgGN])[+]si$/','$1+zi');
+	$text = change('/([iIuUfFxeEoOhyvrlkKgGN][+][i]*)s([ie])$/','$1z$2');
 	storedata('8.3.59','sa',0);
 	$Adezapratyaya=1;
 }
