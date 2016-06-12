afbbd76e9811042b01fef39073881522b62c4954
drdhaval2785 <drdhaval2785@gmail.com>
2016-06-12 16:30:12 +0530

    SiSUH -> SiSuvuH

--- scripts/function.php
+++ scripts/function.php
@@ -1491,8 +1491,8 @@ function samprasarana($input,$merge)
 	{
 		$text = two(array("i","u","f","x"),$ac,array("i","u","f","x"),blank(count($ac)),2);
 		storedata('6.1.108','sa',0);
-	}   
-	if (sub(array("u+uc","i+ij","u+up","u+ud","u+uh","u+uS","u+us"),blank(0),blank(0),0))
+	}
+	if ( (arr($text,'/^i[+]i/')||arr($text,'/^u[+]u/')) && sub(array("u+uc","i+ij","u+up","u+ud","u+uh","u+uS","u+us"),blank(0),blank(0),0))
 	{
 		$text = one(array("u+uc","i+ij","u+up","u+ud","u+uh","u+uS","u+us"),array("Uc","Ij","Up","Ud","Uh","US","Us"),0);
 		storedata('6.1.101','sa',0);
