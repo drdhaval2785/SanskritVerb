8a1b736e21c7cd05acf894d99a319a41b21de2d7
drdhaval2785 <drdhaval2785@gmail.com>
2016-06-20 10:04:16 +0530

    documenation of scripts over

--- scripts/verblistgenerator.php
+++ scripts/verblistgenerator.php
@@ -1,20 +1,32 @@
 <?php
+/* 
+	Usage: `php verblistgenerator.php`
+	Script to generate two variables $verbsingerard and $verblist from ../Data/verblist_gerard.txt.
+	This script will generate the verbs available in Gerard's XML database.
+	It will also generate the list of verb forms included in INRIA, UoHyd, okforms and notnow lists.
+*/
 /* set execution time to an hour */
 ini_set('max_execution_time', 36000);
 /* set memory limit to 100000 MB */
 ini_set("memory_limit","100000M");
 include 'function.php';
+# Create a controllist
 $controllist=vl();
+# Echo it
 echo '$verbsingerard=array("'.implode('","',$controllist).'");';
+# Create a verbform list from four inputs.
 $verblist=verbforms();
+# Echo it
 echo '$verblist=array("'.implode('","',$verblist).'");';
 
+// Read the comma separated values in an array
 function vl()
 {
 	$inputtext = file_get_contents('../Data/verblist_gerard.txt');
 	$controllist = explode(',',$inputtext);
 	return $controllist;
 }
+// Function to scrape the verb forms from ../Data/verbforms_amba.txt, ../Data/verbforms_gerard.txt, ../Data/okforms.txt and ../Data/notnow.txt
 function verbforms()
 {
 	$inputtext = file_get_contents('../Data/verbforms_gerard.txt');
@@ -27,7 +39,13 @@ function verbforms()
 	{
 		$okfinal[] = explode('-',$okp)[0];
 	}
-	$finallist = array_merge($controllist,$controllist1,$okfinal);
+	$notnowprelim = file('../Data/notnow.txt');
+	$notnowfinal=array();
+	foreach ($notnowprelim as $nop)
+	{
+		$notnowfinal[] = explode('-',$nop)[0];
+	}
+	$finallist = array_merge($controllist,$controllist1,$okfinal,$notnowfinal);
 	return $finallist;
 }
 
--- scripts/wxtoslp.py
+++ scripts/wxtoslp.py
@@ -1,18 +1,37 @@
+"""
+Expected outcome:
+	Convert a file from WX encoding to SLP1 encoding.
+	The code can be called as module like `from wxtoslp import wxtoslp` and then used as a function.
+Usage:
+	python wxtoslp.py inputfile outputfile
+
+"""
 import string
 import sys
+# Function to convert an input from WX to SLP1 encoding
 def wxtoslp(input):
+	# Create a character set of both WX and SLP1
 	wx = 'aAiIuUqQLeEoOMHhyvrlFmfNnJBGDXjbgdxKPCTWctwkpSRsz'
 	slp = 'aAiIuUfFxeEoOMHhyvrlYmNRnJBGQDjbgqdKPCWTcwtkpSzs!'
+	# Create a translation table
 	trantab = string.maketrans(wx,slp)
+	# Return the converted string
 	return input.translate(trantab)
-#print wxtoslp('akR1_akRUz_BvAxiH vyApwO')
+
+# Code for file conversions
 if __name__=="__main__":
+	# Read arguments in to infile and outfile
 	infile = sys.argv[1]
 	outfile = sys.argv[2]
+	# Read inputfile
 	fin = open(infile,'r')
 	data = fin.read()
+	# Convert to SLP1
 	slpdata = wxtoslp(data)
+	# Open outputfile
 	fout = open(outfile,'w')
+	# Write to outputfile
 	fout.write(slpdata)
+	# Close files
 	fin.close()
 	fout.close()
