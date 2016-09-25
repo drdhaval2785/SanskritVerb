8aabf1d5d6fde9d355a651a9943ee07c5b7bef26
drdhaval2785 <drdhaval2785@gmail.com>
2016-09-25 22:28:20 +0530

    968 data regenerated after corrections in verbdata

--- scripts/issue968.php
+++ scripts/issue968.php
@@ -1,11 +1,12 @@
 <?php
-// Find discrepancies in gaNa 10 as per github issue 967.
+// Find discrepancies in gaNa 10 as per github issue 968.
 include('function.php');
 foreach($verbdata as $verbdatum)
 {
 	list($verb[],$meaning[],$verb1[],$verbset[],$verbnumber[],$verbpada[],$verbit[],$deva[],$ma[],$ks[],$dh[],$uohyd[],$jnu[]) = explode(':',$verbdatum);
 }
 $setlist = array("01","02","03","04","05","06","07","08","09","10");
+$fout = fopen('../Data/issue968/968.txt','w','utf-8');
 foreach($setlist as $setvalue)
 {
 	$arr = array();
@@ -23,10 +24,12 @@ foreach($setlist as $setvalue)
 		if(!in_array($val-1,$arr) && $val!==1)
 		{
 			echo $setvalue.":".(string)($val-1)."\n";
+			fputs($fout,$setvalue.":".(string)($val-1)."\n");
 		}
 		elseif(!in_array($val+1,$arr) && $val!==1)
 		{
 			echo $setvalue.":".(string)($val+1)."\n";
+			fputs($fout,$setvalue.":".(string)($val+1)."\n");
 		}
 	}
 }
