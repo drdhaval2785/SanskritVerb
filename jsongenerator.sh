a9babc77567075d10a9adbde2bc9ecf5c4f3c2df
drdhaval2785 <drdhaval2785#gmail.com>
2017-12-15 10:52:35 +0530

    rm in jsongenerator

--- jsongenerator.sh
+++ jsongenerator.sh
@@ -1,8 +1,11 @@
+!#/usr/bin/bash
 timestamp() {
   date +"%d-%m-%Y %H:%M:%S"
 }
 LIST2=(law liw luw lfw low laN ASIrliN viDiliN luN lfN)
 source Data/list1.txt
+echo "Removing the existing json files. $(timestamp)"
+rm -f json/*.json
 for VALUE1 in "${LIST1[@]}"
 do
 	echo "$number - processing verb number $VALUE1 started at $(timestamp)"
